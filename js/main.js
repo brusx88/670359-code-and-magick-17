9d93ab3d171c7632728c17935de6d80f4b8bee0b
Vadim Brusinsky <brusinskij13@gmail.com>
2019-06-02 13:53:51 +0400

    2. Учебный проект: начинаем программировать

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+var getWizardX = function (width) {
+  return 300;
+};
+var getWizardY = function (height) {
+  return 150;
+};
+var getWizardHeight = function () {
+  return 1.337 * wizardWidth;
+};
+var getFireballSpeed = function (left) {
+  if (left) {
+    return 5;
+  } return 2;
+};
+
